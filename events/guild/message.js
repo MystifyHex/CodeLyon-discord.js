b6f764a18d373c82294353347fdbb19b69379008
MystifyHex <73503199+MystifyHex@users.noreply.github.com>
2021-02-14 14:39:18 -0600

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 module.exports = (Discord, client, message) =>{
-    const prefix = '!';
+    const prefix = process.env.PREFIX;
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
