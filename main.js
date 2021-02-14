68d194c7b2d8a41f9e73e74976ef081c8d1fd428
MystifyHex <73503199+MystifyHex@users.noreply.github.com>
2021-02-14 13:50:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Active')
-});
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-client.login('Your Token Here')
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord)
+})
+
+client.login('Your Token Here!');
